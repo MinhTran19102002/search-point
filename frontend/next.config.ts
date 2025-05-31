9615e98d68f53a0dc9c8eb5d5ec3422b88a4c248
MinhTran19102002 <20110677@student.hcmute.edu.vn>
2025-05-31 19:36:16 +0700

    deploy fe

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,5 +3,16 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
 };
+module.exports = {
+  async redirects() {
+    return [
+      {
+        source: '/',
+        destination: '/dashboard',
+        permanent: false, // đổi thành true nếu muốn redirect 301
+      },
+    ]
+  },
+}
 
 export default nextConfig;
